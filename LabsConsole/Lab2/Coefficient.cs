05644dca303964cadefcf0563524ae38a445a138
Arctic Beaver <ArcticBeaver@yandex.ru>
2022-02-20 19:05:46 +0300

    Lab2 in process

---
+++ LabsConsole/Lab2/Coefficient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsConsole
+{
+    public class Coefficient
+    {
+        public double Denominator { get; set; } //Знаменатель
+        public List<double> NumeratorPolinom { get; set; } //Числитель в виде a0 = num[0], a1 = num[1]...
+
+        public Coefficient(List<double> x)
+        {
+            Denominator = new double();
+            NumeratorPolinom = new List<double>();        
+
+            CountDenominator(x);
+            CountNumerator(x);
+        }
+
+        public void CountDenominator(List<double> x)
+        {
+
+        }
+
+        public void CountNumerator(List<double> x)
+        {
+
+        }
+    }
+}
---
+++ LabsConsole/Lab2/Lab2Console.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabsConsole
+{
+    public class Lab2Console
+    {
+		//Интерполирование и экстраполирование данных.
+		//Интерполяционный многочлен Лагранжа
+
+		//В-т 19
+
+		List<double> enteredX;
+		List<double> enteredY;
+		List<double> coefficients;
+
+		private int amountOfValues;
+
+		public double[,] matrixCoef { get; set; }
+
+		public Lab2Console(List<double> x, List<double> y)
+        {
+			enteredX = x;
+			enteredY = y;
+
+			amountOfValues = Math.Min(enteredY.Count, enteredX.Count);
+
+			coefficients = new List<double>();
+			matrixCoef = new double[x.Count, x.Count];
+		}
+
+		public void FillMatrix(List<double> x)
+		{
+			for (int i = 0; i < amountOfValues; i++)
+            {
+				for (int j = 0; j < amountOfValues; i++)
+                {
+					if (i == j) matrixCoef[i, j] = -enteredX[i]; //это диагональ
+					else
+                    {
+						matrixCoef[i, j] = enteredX[i] - enteredX[j];
+					}
+                }
+            }
+		}
+
+
+
+    }
+}
+
+
