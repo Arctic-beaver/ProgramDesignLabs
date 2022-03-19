d5f56198063b1c4c7570effd6d679f5bc0a0b8fe
Arctic Beaver <ArcticBeaver@yandex.ru>
2022-03-19 16:49:00 +0300

    Lab3 in progress

--- LabsConsole/Lab3Console.cs
+++ LabsConsole/Lab3Console.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace LabsConsole
 {
-    public class Lab3Console
-    {
-    }
+	public class Lab3Console
+	{
+		double xt;
+		double[] enteredX;
+		double[] enteredY;
+		double[,] TableOfDifferences;
+        int amountOfElements;
+
+
+		public Lab3Console(double[] X, double[] Y, double Xt)
+        {
+			xt = Xt;
+			enteredX = X;
+			enteredY = Y;
+			amountOfElements = Math.Min(X.Length, Y.Length);
+			TableOfDifferences = new double[amountOfElements, amountOfElements];
+		}
+
+		public void DoLab3()
+        {
+			FillTableOffDifferences();
+
+        }
+
+		void FillTableOffDifferences()
+        {
+            for (int i = 0; i < amountOfElements; i++)
+            {
+                TableOfDifferences[0, i] = enteredY[i];
+            }
+
+			for (int down = 1; down < amountOfElements; down++)
+            {
+				for (int left = 1; left < amountOfElements; left++)
+                {
+					TableOfDifferences[left, down] = TableOfDifferences[left - 1, down] - TableOfDifferences[left - 1, down - 1];
+                }
+            }
+        }
+	}
 }
--- LabsConsole/Program.cs
+++ LabsConsole/Program.cs
@@ -9,4 +9,9 @@ Lab1Console lab1 = new Lab1Console(15, 200, 0.95, 10, 0.005, 0.001, 3, 0.01, 0.0
 Lab2Console lab2 = new Lab2Console(new double[]{ 1.45, 1.36, 1.14 }, 
                              new double[]{ 3.14, 4.15, 5.65 });
 
-lab2.DoLab2();
+//lab2.DoLab2();
+
+Lab3Console lab3 = new Lab3Console(new double[] { 1.01, 1.08, 1.11, 1.21, 1.26, 1.33, 1.46, 1.51 },
+    new double[] { 12.6183, 12.7644, 12.9122, 13.0617, 13.2130, 13.3660, 13.5207, 13.8357 }, 1.488);
+
+lab3.DoLab3();
